59a9337b0dba73e48d54f0bcf7c83ac2de85dfe5
guido offermans <guidooffermans@hotmail.com>
2022-05-20 17:39:39 +0200

    add the router and the routes

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get( '/', fn() => view( 'home' ) )->name( 'home' );
+Route::get( 'about', fn() => view( 'about' ) )->name( 'about' );
+Route::get( 'atelier-spaces', fn() => view( 'atelier-spaces' ) )->name( 'atelier-spaces' );
+Route::get( 'stammtisch', fn() => view( 'stammtisch' ) )->name( 'stammtisch' );
+Route::get( 'events', fn() => view( 'events' ) )->name( 'events' );
+Route::get( 'portraits', fn() => view( 'portraits' ) )->name( 'portraits' );
+Route::get( 'kontakt', fn() => view( 'kontakt' ) )->name( 'kontakt' );
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/dashboard', function () {
+    return view( 'dashboard' );
+} )->middleware( [ 'auth' ] )->name( 'dashboard' );
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
