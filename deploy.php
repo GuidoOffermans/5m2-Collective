e74a56c729450abf9d7fd5647826baf8995c940c
guido offermans <guidooffermans@hotmail.com>
2022-05-25 20:55:16 +0200

    add deployer deply.php

---
+++ deploy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set( 'repository', 'https://github.com/GuidoOffermans/5m2-Collective.git' );
+
+add( 'shared_files', [] );
+add( 'shared_dirs', [] );
+add( 'writable_dirs', [] );
+
+// Hosts
+
+host( '194.13.82.194' )
+    ->set( 'remote_user', 'atheros' )
+    ->set( 'deploy_path', '~/apps/5m2-Collective' );
+
+// Tasks
+
+task( 'build', function () {
+    cd( '{{release_path}}' );
+    run( 'npm run build' );
+} );
+
+after( 'deploy:failed', 'deploy:unlock' );
