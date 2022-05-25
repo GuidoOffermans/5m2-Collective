99d44da544c2f4c55fd34df59858311093c7cf04
guido offermans <guidooffermans@hotmail.com>
2022-05-25 21:39:14 +0200

    make deploy more clear

--- deploy.php
+++ deploy.php
@@ -15,8 +15,8 @@ add( 'writable_dirs', [] );
 // Hosts
 
 host( '194.13.82.194' )
-    ->set( 'remote_user', 'atheros' )
-    ->set( 'deploy_path', '~/apps/5m2-Collective' );
+    ->setRemoteUser( 'atheros' )
+    ->setDeployPath( '~/apps/5m2-Collective' );
 
 // Tasks
 
